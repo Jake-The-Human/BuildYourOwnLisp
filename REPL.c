1ef8ca0ad85adbdf579be5bc3bb532d1a8689830
Jake-The-Human <jakepoint2@gmail.com>
2017-03-13 19:40:03 -0400

    now evaluates input

--- REPL.c
+++ REPL.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h> // for free func
+#include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32 // this macro is define if this code is running on a win 32 or 64
 #include <string.h>
@@ -13,7 +14,7 @@ char* readline(char* prompt) {
   // for null byte
   char* cpy = malloc(strlen(buffer)+1);
   strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\n';
+  cpy[strlen(cpy)-1] = '\0';
   return cpy;
 }
 
@@ -29,27 +30,82 @@ void add_history(char* unsuded) {}
 // static char input[2048];
 //using rediline history now to store input dynamicly
 
+long eval_op(long x, char* op, long y) {
+  if (!strcmp(op, "+")) { return x + y; }
+  if (!strcmp(op, "-")) { return x - y; }
+  if (!strcmp(op, "*")) { return x * y; }
+  if (!strcmp(op, "/")) { return x / y; }
+  return 0;
+}
+
+long eval (mpc_ast_t* t) {
+  // base case
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  // operator is always the secound child
+  char* op = t->children[1]->contents;
+  // evaluate expr arg
+  long x = eval(t->children[2]);
+  // iterate the remaining children and combine
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  return x;
+}
+
+
 int main(int argc, char** argv){
+  // create parsers
+   mpc_parser_t* Number   = mpc_new("number");
+   mpc_parser_t* Operator = mpc_new("operator");
+   mpc_parser_t* Expr     = mpc_new("expr");
+   mpc_parser_t* Lispish  = mpc_new("lispish");
+
+  //define them with the following language
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+(\\.[0-9]*)?/;                  \
+      operator : '+' | '-' | '*' | '/' | '%';             \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispish    : /^/ <operator> <expr>+ /$/ ;           \
+    ",
+    Number, Operator, Expr, Lispish);
 
   /*Prints the version and how to exit*/
-  puts("Lispish Version 0.0.0.0.1");
+  puts("Lispish Version 0.0.0.4.2");
   puts("Made in the USA, By Jake Adamson");
   puts("Press Ctrl+c or :q to Exit\n");
 
   for(;;){
-    // prompts user
+
     char* input = readline("lispish> ");
-    // add input to history
     add_history(input);
-    if(input[0] == ':' && input[1] == 'q') {
+
+    if(!strcmp(input, ":q")) {
         free(input);
         break;
       }
-    // prints what you said back to you
-    printf("%ssssssss\n", input);
 
+    /* Attempt to parse the user input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispish, &r)) {
+        /* On success print and delete the AST */
+        long result = eval(r.output);
+        printf("%li\n", result);
+        mpc_ast_delete(r.output);
+      } else {
+        /* Otherwise print and delete the Error */
+        mpc_err_print(r.error);
+        mpc_err_delete(r.error);
+      }
     // free the input b/c its stored on heap
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispish);
+
   return 0;
 }
--- parsing.c
+++
@@ -1,6 +0,0 @@
-#include "mpc.h"
-
-int main(int argc, char** argv){
-
-    return 0;
-}
